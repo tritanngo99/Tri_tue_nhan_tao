465b449e4684c8bbd446f66bee01dfeec54a6022
tritanngo99 <49904863+tritanngo99@users.noreply.github.com>
2020-09-30 05:54:24 +0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,11 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+	Point p = null;
+	int value;
+	Node cha = null;
+	List<Node> con = new ArrayList<>();
+	
+}
